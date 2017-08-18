eab892c5c0936b8ec57b85e01d6ae53eb3341c74
Sense Luo <gluo@jindouyun.com>
2017-08-18 15:00:58 +0800

    change greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'greeting' });
+  res.send({ hi: 'yalevanrental' });
 });
 
 const PORT = process.env.PORT || 5000;
