7fee91811176a1f213c452eda59a4e6a772fb35c
Sense Luo <gluo@jindouyun.com>
2017-08-18 23:57:23 +0800

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   googleClientID:
     '222513016693-1nf93g7tjekn3ca6aphdffu7v30t3s5h.apps.googleusercontent.com',
-  googleClientSecret: 'y8Qr_eMnXumccgbyOwut-Xdh'
+  googleClientSecret: 'gd72vPjt9fcU5oAVIuDhXHNi'
 };
