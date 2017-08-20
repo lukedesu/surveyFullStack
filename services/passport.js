6b80c63a50d0dea77da27d8369cf64ec52f60ce1
Sense Luo <gluo@jindouyun.com>
2017-08-20 18:09:35 +0800

    tweak proxy issue

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
@@ -46,7 +47,8 @@ passport.use(
     {
       clientID: keys.facebookClientID,
       clientSecret: keys.facebookClientSecret,
-      callbackURL: '/auth/facebook/callback'
+      callbackURL: '/auth/facebook/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(accessToken);
