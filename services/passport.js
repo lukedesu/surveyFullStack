9034ea8a4ab4a8ce706acbed4ace396adc896c17
Sense Luo <gluo@jindouyun.com>
2017-08-20 18:27:30 +0800

    revert fb callback

--- services/passport.js
+++ services/passport.js
@@ -51,18 +51,10 @@ passport.use(
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-    //   User.findOne({ googleId: profile.id }).then(existingUser => {
-    //     if (existingUser) {
-    //       // already has it
-    //       done(null, existingUser);
-    //     } else {
-    //       // new user
-    //       new User({
-    //         googleId: profile.id
-    //       })
-    //         .save()
-    //         .then(user => done(null, user));
-    //     }
-    // }
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+      console.log(done);
+    }
   )
 );
