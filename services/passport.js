0d5afe94733647d58c539d20a9f69564ce5dbfc5
Sense Luo <gluo@jindouyun.com>
2017-08-20 18:34:54 +0800

    add facebook id

--- services/passport.js
+++ services/passport.js
@@ -51,10 +51,19 @@ passport.use(
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-      console.log(done);
+      User.findOne({ facebookId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already has it
+          done(null, existingUser);
+        } else {
+          // new user
+          new User({
+            facebookId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
