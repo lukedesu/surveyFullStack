dc014f0dea42f6f4442c02eb7bf92620697b843b
Sense Luo <gluo@jindouyun.com>
2017-09-07 20:57:42 +0800

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -10,8 +10,8 @@ import App from './components/App';
 import reducers from './reducers';
 
 // development only axios helper
-import axios from 'axios';
-window.axios = axios;
+// import axios from 'axios';
+// window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
